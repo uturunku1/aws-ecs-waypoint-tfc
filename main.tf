1b91654a25c5ca3e3dc37b5f9da5970c11c682e8
uturunku1 <luces.huayhuaca@gmail.com>
2022-03-10 10:53:45 -0800

    attempting to run terraform import aws_default_security_group.tf_waypoint_security_default sg-60170812 but not working yet

--- main.tf
+++ main.tf
@@ -46,6 +46,29 @@ output "wp_iam_role_name" {
   value       = aws_iam_role.tf_waypoint_iam_role.id
 }
 
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "172.31.0.0/16"
+}
+#attempting to run terraform import aws_default_security_group.tf_waypoint_security_default sg-60170812
+resource "aws_default_security_group" "tf_waypoint_security_default" {
+  vpc_id = aws_vpc.mainvpc.id
+  # name        = "example-nodejs-inbound-internal" //or should it be the one provided by the external security group "example-nodejs-inbound"
+  description = "Allow TLS inbound traffic"
+}
+
+output "wp_security_group_internal" {
+  description = "output the name of security internal group"
+  value       = aws_default_security_group.tf_waypoint_security_default.id
+}
+
+#https://support.hashicorp.com/hc/en-us/articles/360061289934-How-to-Import-Resources-into-a-Remote-State-Managed-by-Terraform-Cloud
+# Done:
+# terraform import aws_ecs_cluster.tf_waypoint_cluster waypoint
+# terraform import aws_cloudwatch_log_group.tf_waypoint_logs waypoint-logs
+# terraform import aws_iam_role.tf_waypoint_iam_role ecr-example-nodejs
+
+# Speculative plans are not listed the platform's UI. But the url to each of those plans can be found in the PR's checks that ran after each commit.
+
 #Other outputs needed are things that come from these create methods.
 #Noting here so we can work through them
 # resourceClusterCreate
@@ -56,17 +79,6 @@ output "wp_iam_role_name" {
 # resourceServiceSubnetsDiscover
 # resourceAlbSubnetsDiscover
 # resourceAlbCreate
-
-#https://support.hashicorp.com/hc/en-us/articles/360061289934-How-to-Import-Resources-into-a-Remote-State-Managed-by-Terraform-Cloud
-# Done:
-# terraform import aws_ecs_cluster.tf_waypoint_cluster waypoint
-# terraform import aws_cloudwatch_log_group.tf_waypoint_logs waypoint-logs
-
-
-# resource "aws_security_group" "allow_tls" {
-#   name        = "example-nodejs-inbound-internal" //or should it be the one provided by the external security group "example-nodejs-inbound"
-#   description = "Allow TLS inbound traffic"
-# }
 
 
 
