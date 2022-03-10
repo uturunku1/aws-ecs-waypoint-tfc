ab1b9c1d57f4fc36810323f46cf5a8cd6e569c79
uturunku1 <luces.huayhuaca@gmail.com>
2022-03-10 11:52:05 -0800

    import security groups from correct region

--- main.tf
+++ main.tf
@@ -37,44 +37,59 @@ output "wp_cloudwatch_logs_name" {
   value       = aws_cloudwatch_log_group.tf_waypoint_logs.id
 }
 
-# resource "aws_iam_role" "tf_waypoint_iam_role" {
-#   name = "ecr-example-nodejs"
-# }
+resource "aws_iam_role" "tf_waypoint_iam_role" {
+  name = "ecr-example-nodejs"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-# output "wp_iam_role_name" {
-#   description = "output the name of my execution IAM role"
-#   value       = aws_iam_role.tf_waypoint_iam_role.id
-# }
-
-data "aws_security_group" "selected" {
-  id = "sg-60170812"
+output "wp_iam_role_name" {
+  description = "output the name of my execution IAM role"
+  value       = aws_iam_role.tf_waypoint_iam_role.id
 }
 
 
-# resource "aws_vpc" "main" {
-#   cidr_block = "172.31.0.0/16"
-# }
+resource "aws_vpc" "inbound_internal" {
+  cidr_block = "172.31.0.0/16"
+}
 
-# resource "aws_security_group" "tf_waypoint_security_default" {
-#   name        = "example-nodejs-inbound-internal"
-#   vpc_id      = aws_vpc.main.id
-# }
+resource "aws_default_security_group" "tf_waypoint_inbound_internal" {
+  vpc_id = aws_vpc.inbound_internal.id
+  # name        = "example-nodejs-inbound-internal"
+}
 
-# resource "aws_default_security_group" "tf_waypoint_security_default" {
-#   vpc_id = aws_vpc.main.id
-#   # name        = "example-nodejs-inbound-internal"
-# }
+resource "aws_default_security_group" "tf_waypoint_inbound" {
+  vpc_id = aws_vpc.inbound_internal.id
+  # name        = "example-nodejs-inbound"
+}
 
-# output "wp_security_group" {
-#   description = "output the name of security internal group"
-#   value       = aws_default_security_group.tf_waypoint_security_default.id
-# }
+output "wp_security_inbound_internal" {
+  value       = aws_default_security_group.tf_waypoint_inbound_internal.id
+}
+output "wp_security_inbound" {
+  value       = aws_default_security_group.tf_waypoint_inbound.id
+}
 
 #https://support.hashicorp.com/hc/en-us/articles/360061289934-How-to-Import-Resources-into-a-Remote-State-Managed-by-Terraform-Cloud
 # Done:
 # terraform import aws_ecs_cluster.tf_waypoint_cluster waypoint
 # terraform import aws_cloudwatch_log_group.tf_waypoint_logs waypoint-logs
 # terraform import aws_iam_role.tf_waypoint_iam_role ecr-example-nodejs
+# terraform import aws_default_security_group.tf_waypoint_inbound_internal sg-0166caeecca88094f
+# terraform import aws_default_security_group.tf_waypoint_inbound sg-0dc34cc1d070b7882
 
 # Speculative plans are not listed the platform's UI. But the url to each of those plans can be found in the PR's checks that ran after each commit.
 
